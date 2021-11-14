f33332635a6098478e20c72d36e1ddd082d74f9f
athar05 <atharshakeel96@gmail.com>
2021-11-14 16:12:04 +0530

    added more questions

--- practice.js
+++ practice.js
@@ -6,6 +6,8 @@
 // const divisionBtn = document.getElementById("division");
 // const output = document.getElementById("output");
 
+// 1. Create a web app which would take two inputs. It would also have 4 buttons: +, -, x /. Based on the button clicked perform the operation on the two inputs. You can do this in React or vanillaJS based on your choice. Your time starts now.
+
 // function addInputs() {
 //   const result = Number(input1.value) + Number(input2.value);
 //   output.innerText = result;
@@ -37,6 +39,8 @@
 // SIQ 2 ends
 
 // -------------------------------------------------------------------
+// Create a web app where I can input a text. Now create two buttons + and -. On clicking + increase the fontSize by 2px and vice versa.
+
 // const theText = document.getElementById("text");
 
 // const increaseBtn = document.getElementById("increase");
@@ -62,6 +66,8 @@
 //SIQ 3 ends
 
 //---------------------------------------------------------------------------------
+
+// Create a web app where I can input a text. Now, create three buttons h1, h2, h3. When I click on any of the button, the text should become h1, h2, or h3.
 
 // const inputTxt = document.getElementById("inputText");
 // const h1Btn = document.getElementById("header1");
@@ -90,6 +96,8 @@
 
 //---------------------------------------------------------------------------------
 
+// Create a web app where I can input a text. Now, create three buttons: red, green, blue. Clicking on the button should change the text color.
+
 // const inputText = document.getElementById("text");
 
 // const redBtn = document.getElementById("red");
@@ -117,3 +125,51 @@
 //SIQ5 ends
 
 //---------------------------------------------------------------------------------
+// Create a web app with a button loded. Show a text loading... on a web app. However, hide it if I click on the button loaded.
+
+// const text = document.getElementById("txt");
+// const button = document.getElementById("btn");
+
+// button.addEventListener("click", removeText);
+
+// function removeText() {
+//   text.style.display = "none";
+// }
+
+// -------------------------------------------------------------------------------------
+
+// Here's an API. Create a web app to call this API with your full name and print the response. This could be extended where we ask you to do something with the response. Like add a text, or capitalise etc.
+
+// let url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// function callAPI(text) {
+//   fetch(url + text)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+// }
+
+// callAPI("?text=I am Shaktiman");
+// ------------------------------------------------------------------------------------
+
+// Create two objects with name, age, and yuga as Ram, 25, Treta. Krishna, 31, Dwapar. Write a function which takes two objects and return the person with more age.
+
+// let obj1 = {
+//   name: "Ram",
+//   age: 31,
+//   yuga: "Treta",
+// };
+
+// let obj2 = {
+//   name: "Krishna",
+//   age: 25,
+//   yuga: "Dwapar",
+// };
+
+// function ageChecker(a, b) {
+//   if (a.age > b.age) {
+//     console.log(a.name);
+//   } else {
+//     console.log(b.name);
+//   }
+// }
+// ageChecker(obj1, obj2);
