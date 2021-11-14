2ae443d26a07d0a79f74cca9c36993c19d6c8b67
athar05 <atharshakeel96@gmail.com>
2021-11-14 15:46:01 +0530

    initial commit

---
+++ practice.js
@@ -0,0 +1,119 @@
+// const input1 = document.getElementById("inputOne");
+// const input2 = document.getElementById("inputTwo");
+// const additionBtn = document.getElementById("addition");
+// const subtractionBtn = document.getElementById("subtraction");
+// const multiplicationBtn = document.getElementById("multiplication");
+// const divisionBtn = document.getElementById("division");
+// const output = document.getElementById("output");
+
+// function addInputs() {
+//   const result = Number(input1.value) + Number(input2.value);
+//   output.innerText = result;
+// }
+
+// additionBtn.addEventListener("click", addInputs);
+
+// function subtractInputs() {
+//   const result = Number(input1.value) - Number(input2.value);
+//   output.innerText = result;
+// }
+
+// subtractionBtn.addEventListener("click", subtractInputs);
+
+// function multiplyInputs() {
+//   const result = Number(input1.value) * Number(input2.value);
+//   output.innerText = result;
+// }
+
+// multiplicationBtn.addEventListener("click", multiplyInputs);
+
+// function divideInputs() {
+//   const result = Number(input1.value) / Number(input2.value);
+//   output.innerText = result;
+// }
+
+// divisionBtn.addEventListener("click", divideInputs);
+
+// SIQ 2 ends
+
+// -------------------------------------------------------------------
+// const theText = document.getElementById("text");
+
+// const increaseBtn = document.getElementById("increase");
+
+// const decreaseBtn = document.getElementById("decrease");
+
+// let number = 16;
+
+// function increaseText() {
+//   number = number + 2;
+//   theText.style.fontSize = `${number}px`;
+// }
+
+// increaseBtn.addEventListener("click", increaseText);
+
+// function decreaseText() {
+//   number = number - 2;
+//   theText.style.fontSize = `${number}px`;
+// }
+
+// decreaseBtn.addEventListener("click", decreaseText);
+
+//SIQ 3 ends
+
+//---------------------------------------------------------------------------------
+
+// const inputTxt = document.getElementById("inputText");
+// const h1Btn = document.getElementById("header1");
+// const h2Btn = document.getElementById("header2");
+// const h3Btn = document.getElementById("header3");
+
+// function changeToH1() {
+//   inputTxt.outerHTML = "<H1>" + inputTxt.innerHTML + "</H1>";
+// }
+
+// h1Btn.addEventListener("click", changeToH1);
+
+// function changeToH2() {
+//   inputTxt.outerHTML = "<H2>" + inputTxt.innerHTML + "</H2>";
+// }
+
+// h2Btn.addEventListener("click", changeToH2);
+
+// function changeToH3() {
+//   inputTxt.outerHTML = "<H3>" + inputTxt.innerHTML + "</H3>";
+// }
+
+// h3Btn.addEventListener("click", changeToH3);
+
+//SIQ4 ends
+
+//---------------------------------------------------------------------------------
+
+// const inputText = document.getElementById("text");
+
+// const redBtn = document.getElementById("red");
+// const greenBtn = document.getElementById("green");
+// const blueBtn = document.getElementById("blue");
+
+// function changeRed() {
+//   inputText.style.color = "red";
+// }
+
+// redBtn.addEventListener("click", changeRed);
+
+// function changeGreen() {
+//   inputText.style.color = "green";
+// }
+
+// greenBtn.addEventListener("click", changeGreen);
+
+// function changeBlue() {
+//   inputText.style.color = "blue";
+// }
+
+// blueBtn.addEventListener("click", changeBlue);
+
+//SIQ5 ends
+
+//---------------------------------------------------------------------------------
